e62bf83b932cdec728551f6e594dc2ccd9d21f15
maylis imperiali <imp.mayli@gmail.com>
2026-02-09 19:46:13 +0100

    nf

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Counter from "./components/Counter";
+import { useState } from "react";
 import ExponentTwo from "./components/ExponentTwo";
 import ExponentThree from "./components/ExponentThree";
 import ExponentFour from "./components/ExponentFour";
@@ -7,22 +8,37 @@ import ExponentFive from "./components/ExponentFive";
 import ExponentSix from "./components/ExponentSix";
 
 
+
 function App () {
+
+  
+const [count, setCount] = useState(0);
+    
+      const decrement = () => setCount((prevCount) => prevCount - 1);
+      const increment = () => setCount((prevCount) => prevCount + 1);
   return (
+
+
+
+     
     <div className="App">
       <h2><em>Counter</em></h2>
   
-      <Counter/>
+      <Counter
+      count={count}
+  increment={increment}
+  decrement={decrement}
+/>
 
       <br />
       <h2><em>Exponents</em></h2>
 
       <div className="container">
-        <ExponentTwo />
-        <ExponentThree />
-        <ExponentFour />
-        <ExponentFive />
-        <ExponentSix />
+         <ExponentTwo count={count} />
+        <ExponentThree count={count} />
+        <ExponentFour count={count} />
+        <ExponentFive count={count} />
+        <ExponentSix count={count} />
       </div>
     </div>
   );
